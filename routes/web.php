3340f9a0b7ccdbbcc238a233b7715bd4ade5f3b4
Della Arselatuiqrom <75599380+Dellarselatuiqrom@users.noreply.github.com>
2021-06-03 17:06:23 +0700

    Authentication Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomepageController;
 
 /*
@@ -40,3 +41,7 @@ Route::group(['prefix' => 'admin'], function() {
   // proses laporan
   Route::get('proseslaporan', 'LaporanController@proses');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
