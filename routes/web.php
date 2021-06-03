4c938db6f91a2b3f4f6c67d24c85b3a8430afd43
Della Arselatuiqrom <75599380+Dellarselatuiqrom@users.noreply.github.com>
2021-06-03 16:30:41 +0700

    membuat admin dashboard transaksi

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,6 @@ Route::group(['prefix' => 'admin'], function() {
   Route::resource('produk', 'ProdukController');
   // route customer
   Route::resource('customer', 'CustomerController');
+  // route transaksi
+  Route::resource('transaksi', 'TransaksiController');
 });
