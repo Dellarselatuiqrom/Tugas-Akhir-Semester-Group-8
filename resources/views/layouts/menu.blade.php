763362520ab854dab9858905689b2e4b8f894b4a
Della Arselatuiqrom <75599380+Dellarselatuiqrom@users.noreply.github.com>
2021-06-11 08:56:45 +0700

    nambah navbar data alamat pengiriman

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -48,11 +48,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    
+
                                     <a class="dropdown-item" href="{{ route('transaksi.index') }}">
                                         {{ __('Transaksi') }}
                                     <a>
-
+                                    <a class="dropdown-item" href="{{ route('alamatpengiriman.index') }}">
+                                        {{ __('Data Alamat Pengiriman') }}
+                                    <a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
